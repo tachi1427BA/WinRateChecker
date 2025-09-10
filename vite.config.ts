7a5fbe7b833b29f9e72030ff067bead6c9d1e4a0
k-Tachibana <45709266+KokiTachibanaHS@users.noreply.github.com>
2025-09-11 02:14:27 +0900

    config修正

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/winrate-app/",
+  base: "/WinRateChecker/",
   plugins: [react()],
 })
